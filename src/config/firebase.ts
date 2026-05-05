e6c6ca0b1de84619d0b0639aed501e423d7fd576
buruk yimesgen <buruky@uw.edu>
2026-05-05 11:14:04 -0700

    fixed secret

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAsCyS_ztHXGDxEZxJSRpnHLtQdgjgYn6E',
-  authDomain: 'ecc-manager-aa6a1.firebaseapp.com',
-  projectId: 'ecc-manager-aa6a1',
-  storageBucket: 'ecc-manager-aa6a1.firebasestorage.app',
-  messagingSenderId: '566636614562',
-  appId: '1:566636614562:web:99f3e0c15293bc06dd98fb',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
